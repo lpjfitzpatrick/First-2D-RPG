797fd2f4a63c1a49d693af54c622404abb42fd16
Luke Fitzpatrick <luke_fitzpatrick55@hotmail.com>
2023-01-13 16:24:02 -0330

    Add chest and door objects to map

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -1,6 +1,8 @@
 package main;
 
 import object.KeyObject;
+import object.ChestObject;
+import object.DoorObject;
 
 public class AssetSetter
 {
@@ -22,5 +24,30 @@ public class AssetSetter
 		key1.m_worldX = 23 * m_gp.tileSize;
 		key1.m_worldY = 40 * m_gp.tileSize;
 		m_gp.m_objects[1] = key1;
+
+		KeyObject key2 = new KeyObject();
+		key2.m_worldX = 38 * m_gp.tileSize;
+		key2.m_worldY = 8 * m_gp.tileSize;
+		m_gp.m_objects[2] = key2;
+
+		DoorObject door1 = new DoorObject();
+		door1.m_worldX = 10 * m_gp.tileSize;
+		door1.m_worldY = 11 * m_gp.tileSize;
+		m_gp.m_objects[3] = door1;
+
+		DoorObject door2 = new DoorObject();
+		door2.m_worldX = 8 * m_gp.tileSize;
+		door2.m_worldY = 28 * m_gp.tileSize;
+		m_gp.m_objects[4] = door2;
+
+		DoorObject door3 = new DoorObject();
+		door3.m_worldX = 12 * m_gp.tileSize;
+		door3.m_worldY = 22 * m_gp.tileSize;
+		m_gp.m_objects[5] = door3;
+
+		ChestObject chest1 = new ChestObject();
+		chest1.m_worldX = 10 * m_gp.tileSize;
+		chest1.m_worldY = 7 * m_gp.tileSize;
+		m_gp.m_objects[6] = chest1;
 	}
 }
