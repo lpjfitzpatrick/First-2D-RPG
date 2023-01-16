f22c7da4d55e437b9dcfe8c81638279fc5277abb
Luke Fitzpatrick <luke_fitzpatrick55@hotmail.com>
2023-01-16 13:04:21 -0330

    Update asset setter to provide coords

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -15,39 +15,25 @@ public class AssetSetter
 
 	public void setAssets()
 	{
-		KeyObject key0 = new KeyObject();
-		key0.m_worldX = 23 * m_gp.tileSize;
-		key0.m_worldY = 7 * m_gp.tileSize;
+		KeyObject key0 = new KeyObject(m_gp.tileSize, 23, 7);
 		m_gp.m_objects[0] = key0;
 
-		KeyObject key1 = new KeyObject();
-		key1.m_worldX = 23 * m_gp.tileSize;
-		key1.m_worldY = 40 * m_gp.tileSize;
+		KeyObject key1 = new KeyObject(m_gp.tileSize, 23, 40);
 		m_gp.m_objects[1] = key1;
 
-		KeyObject key2 = new KeyObject();
-		key2.m_worldX = 38 * m_gp.tileSize;
-		key2.m_worldY = 8 * m_gp.tileSize;
+		KeyObject key2 = new KeyObject(m_gp.tileSize, 38, 8);
 		m_gp.m_objects[2] = key2;
 
-		DoorObject door1 = new DoorObject();
-		door1.m_worldX = 10 * m_gp.tileSize;
-		door1.m_worldY = 11 * m_gp.tileSize;
+		DoorObject door1 = new DoorObject(m_gp.tileSize, 10, 11);
 		m_gp.m_objects[3] = door1;
 
-		DoorObject door2 = new DoorObject();
-		door2.m_worldX = 8 * m_gp.tileSize;
-		door2.m_worldY = 28 * m_gp.tileSize;
+		DoorObject door2 = new DoorObject(m_gp.tileSize, 8, 28);
 		m_gp.m_objects[4] = door2;
 
-		DoorObject door3 = new DoorObject();
-		door3.m_worldX = 12 * m_gp.tileSize;
-		door3.m_worldY = 22 * m_gp.tileSize;
+		DoorObject door3 = new DoorObject(m_gp.tileSize, 12, 22);
 		m_gp.m_objects[5] = door3;
 
-		ChestObject chest1 = new ChestObject();
-		chest1.m_worldX = 10 * m_gp.tileSize;
-		chest1.m_worldY = 7 * m_gp.tileSize;
+		ChestObject chest1 = new ChestObject(m_gp.tileSize, 10, 7);
 		m_gp.m_objects[6] = chest1;
 	}
 }
