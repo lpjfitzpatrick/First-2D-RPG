377952bd8de8e316c43bf7b27c652ed6d1c3c8ea
Luke Fitzpatrick <luke_fitzpatrick55@hotmail.com>
2023-01-20 16:54:49 -0330

    Add some spacing > squash

--- src/ui/LogMessage.java
+++ src/ui/LogMessage.java
@@ -14,6 +14,7 @@ public class LogMessage extends AbstractUI implements LogMessageListener
 	final int MESSAGE_WINDOW_LENGTH = 7*m_gp.m_tileSize;
 	Font m_arial_23i = new Font("Arial", Font.ITALIC, 23);
 	Color m_backgroundColor = new Color(192, 192, 192, 200);
+	float alpha = 1.0f;
 
 	public LogMessage(GamePanel gp)
 	{
@@ -32,12 +33,14 @@ public class LogMessage extends AbstractUI implements LogMessageListener
 		g2D.setFont(m_arial_23i);
 		g2D.setColor(m_backgroundColor);
 		int height = 2;
+
 		if (m_message != null)
 		{
 			FontMetrics fontMetric = g2D.getFontMetrics();
 			double messageWidth = fontMetric.getStringBounds(m_message, g2D).getWidth();
 			String firstHalf = m_message;
 			String secondHalf = null;
+
 			if (messageWidth > MESSAGE_WINDOW_LENGTH)
 			{
 				height = 3;
@@ -46,6 +49,7 @@ public class LogMessage extends AbstractUI implements LogMessageListener
 				firstHalf = m_message.substring(0, splitPoint);
 				secondHalf = m_message.substring(splitPoint);
 			}
+
 			g2D.fillRect(m_gp.m_tileSize/2, 10*m_gp.m_tileSize + 3*m_gp.m_tileSize/4, MESSAGE_WINDOW_LENGTH, height*m_arial_23i.getSize());
 			g2D.setColor(Color.BLACK);
 			g2D.drawString(firstHalf, m_gp.m_tileSize/2, 11*m_gp.m_tileSize + m_gp.m_tileSize/5);
